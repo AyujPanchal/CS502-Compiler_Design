6891c97165f75cff473db84e3fcf2a6067131f60
AyujPanchal <b19099@students.iitmandi.ac.in>
2021-10-19 12:57:40 +0530

    4 testcases passed

--- A3/Main.java
+++ A3/Main.java
@@ -5,7 +5,8 @@ public class Main {
    public static void main(String [] args) {
       try {
          Node root = new PoohPJ(System.in).Goal();
-         root.accept(new GJDepthFirst(), null); // Your assignment part is invoked here.
+         root.accept(new GJDepthFirst1(), null);
+//         root.accept(new GJDepthFirst(), null); // Your assignment part is invoked here.
       }
       catch (ParseException e) {
          System.out.println(e.toString());
